123fda14f7f03b0582618771c91e95b844333026
fgwsz <3191000458@qq.com>
2023-04-28 17:59:26 +0800

    change impl of memory_stack

--- src/memory_stack.c
+++ src/memory_stack.c
@@ -17,8 +17,10 @@ void* memoryStackAlloc(size_t byte_size){
     size_t block_byte_size=0;
     size_t block_begin_index=0;
     while(current_index<stack_capacity){
+        // check byte size of the unused block
         if(memory_stack[current_index]==0){
             block_begin_index=current_index;
+            block_byte_size=0;
             ++block_byte_size;
             ++current_index;
             while(current_index<stack_capacity&&
@@ -26,6 +28,7 @@ void* memoryStackAlloc(size_t byte_size){
                 ++block_byte_size;
                 ++current_index;
             }
+            // the block large enough
             if(block_byte_size>=sizeof(stack_memory_block_head_t)+byte_size){
                 stack_memory_block_head_t* block_head=(stack_memory_block_head_t*)(
                     (unsigned char*)memory_stack+block_begin_index
@@ -33,13 +36,18 @@ void* memoryStackAlloc(size_t byte_size){
                 block_head->flag_=1;
                 block_head->size_=byte_size;
                 stack_current_size-=byte_size+sizeof(stack_memory_block_head_t);
-                return (unsigned char*)block_head+sizeof(stack_memory_block_head_t);
+                void* ret=(unsigned char*)block_head+sizeof(stack_memory_block_head_t);
+                memset(ret,0,byte_size);
+                return ret;
+            // the block not big enough, find the next
             }else if(current_index<stack_capacity){
                 continue;
+            // no available block found
             }else{
                 return NULL;
             }
         }
+        // jump the block in use
         current_index+=current_index+(size_t)(memory_stack[current_index+1]);
     }
 }
