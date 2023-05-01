bcfddbb3c8b6cb8cafb608ee5e7f763dee862f78
fgwsz <3191000458@qq.com>
2023-05-01 17:05:45 +0800

    change impl of memory stack

--- src/main.c
+++ src/main.c
@@ -1,12 +1,13 @@
 #include<stdio.h> // printf
-#include"memory_stack.h"
+#include"memory.h" // memory_alloc memory_free
+#include"memory_stack.h" // memory_stack_print
 int main(void){
-    int* num1=memory_stack_alloc(sizeof(int));
-    int* num2=memory_stack_alloc(sizeof(int));
+    int* num1=memory_alloc(sizeof(int));
+    int* num2=memory_alloc(sizeof(int));
     memory_stack_print();
-    memory_stack_free(num1);
+    memory_free(num1);
     memory_stack_print();
-    memory_stack_free(num2);
+    memory_free(num2);
     memory_stack_print();
     return 0;
 }
--- src/memory_stack.c
+++ src/memory_stack.c
@@ -1,7 +1,7 @@
 #include"memory_stack.h"
 #include<stdio.h> // printf
 typedef unsigned char __byte_t;
-enum{__memory_stack_size=(size_t)(1024*1024)};
+#define __memory_stack_size ((size_t)(1024*1024))
 static __byte_t __memory_stack[__memory_stack_size]={0};
 #pragma pack(push)
 #pragma pack(1)
@@ -26,6 +26,7 @@ typedef struct{
     (__byte_t*)__memory_stack+__memory_stack_size \
 ))
 static __block_head_t* __block_head_first_unused=__block_head_begin;
+static size_t __memory_stack_remaining_size=__memory_stack_size-__block_head_size-__block_tail_size;
 static bool __block_head_getIsUsed(__block_head_t* block_head);
 static void __block_head_setIsUsed(__block_head_t* block_head,bool is_used);
 static size_t __block_head_getBodySize(__block_head_t* block_head);
@@ -186,6 +187,9 @@ extern void* memory_stack_alloc(size_t byte_size){
     if(byte_size==0){
         return NULL;
     }
+    if(byte_size>__memory_stack_remaining_size){
+        return NULL;
+    }
     __block_head_t* block_head=__block_head_first_unused;
     bool is_first_unused=true;
     while(block_head<__block_head_end){
@@ -196,6 +200,7 @@ extern void* memory_stack_alloc(size_t byte_size){
             }
             if(__block_head_getBodySize(block_head)>=byte_size){
                 __block_head_split(block_head,byte_size);
+                __memory_stack_remaining_size-=__block_head_getBodySize(block_head);
                 return __block_head_getBody(block_head);
             }
         }
@@ -237,6 +242,7 @@ extern void memory_stack_free(void* pointer){
     if(__block_head_getIsUsed(block_head)==false){
         return;
     }
+    __memory_stack_remaining_size+=__block_head_getBodySize(block_head);
     if(
         __block_head_hasPrev(block_head)&&
         __block_head_getIsUsed(__block_head_getPrevHead(block_head))==false
@@ -260,26 +266,28 @@ extern void memory_stack_print(){
     __block_head_t* block_head=__block_head_begin;
     size_t count=0;
     __block_tail_t* block_tail=NULL;
+    printf("memory stack begin\n");
     while(block_head<__block_head_end){
-        printf("block [%d] begin\n",count);
+        printf("\tblock [%zu] begin\n",count);
         // head
-        printf("\thead begin\n");
-        printf("\thead:%x\n",(__byte_t*)block_head);
-        printf("\t\tblock_is_used_:%d\n",(int)__block_head_getIsUsed(block_head));
-        printf("\t\tblock_body_size_:%d\n",__block_head_getBodySize(block_head));
-        printf("\thead end\n");
+        printf("\t\thead begin\n");
+        printf("\t\thead:%p\n",block_head);
+        printf("\t\t\tblock_is_used_:%d\n",(int)__block_head_getIsUsed(block_head));
+        printf("\t\t\tblock_body_size_:%zu\n",__block_head_getBodySize(block_head));
+        printf("\t\thead end\n");
         // body
-        printf("\tbody:%x\n",__block_head_getBody(block_head));
+        printf("\t\tbody:%p\n",__block_head_getBody(block_head));
         // tail
         block_tail=__block_head_getTail(block_head);
-        printf("\ttail begin\n");
-        printf("\ttail:%x\n",(__byte_t*)block_tail);
-        printf("\t\tblock_body_size_:%d\n",__block_tail_getBodySize(block_tail));
-        printf("\t\tblock_is_used_:%d\n",(int)__block_tail_getIsUsed(block_tail));
-        printf("\ttail end\n");
-        printf("block [%d] end\n",count);
+        printf("\t\ttail begin\n");
+        printf("\t\ttail:%p\n",block_tail);
+        printf("\t\t\tblock_body_size_:%zu\n",__block_tail_getBodySize(block_tail));
+        printf("\t\t\tblock_is_used_:%d\n",(int)__block_tail_getIsUsed(block_tail));
+        printf("\t\ttail end\n");
+        printf("\tblock [%zu] end\n",count);
         // next block
         block_head=__block_head_getNextHead(block_head);
         ++count;
     }
+    printf("memory stack end\n");
 }
